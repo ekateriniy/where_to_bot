9a527a267416f405aee788c9722ba42dc11e5678
ekateriniy <ekaterin.yu@gmail.com>
2022-09-13 14:00:08 +0800

    refactor response formatting

--- app/controllers/api/webhooks_controller.rb
+++ app/controllers/api/webhooks_controller.rb
@@ -17,12 +17,23 @@ class Api::WebhooksController < Telegram::Bot::UpdatesController
   def response_message
     return t('telegram.http_error') unless @response
     
-    # select first 5 places, since they are already sorted in gmaps by prominence
-    result = ""
-    @response['results'][0..4].each_with_index do |res, i|
-      result << "#{i + 1}.  #{res['name']}, rating: #{res['rating']} (#{res['user_ratings_total']} reviews)\n" if res['rating'] >= 3.5
+    result_string = ""
+    result.each_with_index do |res, i|
+      result_string << "#{i + 1}. #{res['name']}, rating: #{res['rating']} (#{res['user_ratings_total']} reviews)\n"
     end
 
-    "Result:\n#{result}"
+    "Result:\n#{result_string}"
+  end
+
+  def result
+    # select first 5 places, since they are already sorted in gmaps by prominence
+    result = []
+    @response['results'].each do |res|
+      result << res if res['rating'] >= 3.5 && res['user_ratings_total'] > 500
+
+      break if result.size >= 5
+    end
+
+    result
   end
 end
