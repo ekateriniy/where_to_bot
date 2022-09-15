b124d5301cb5e73cbdcd5d96babe91b4c3f8829b
ekateriniy <ekaterin.yu@gmail.com>
2022-09-15 12:27:54 +0800

    increase search radius

--- app/services/map_search_service.rb
+++ app/services/map_search_service.rb
@@ -2,21 +2,32 @@ require "uri"
 require "net/http"
 
 class MapSearchService
+  MAX_RADIUS = 1000
+
   def initialize(location, radius = 200)
     @latitude = location['latitude']
     @longitude = location['longitude']
     @radius = radius
-    @result = {}
   end
 
   def call
-    result = make_request
-    json_result(result)
+    search_cycle
   rescue StandardError
     false
   end
 
   private
+
+  def search_cycle
+    while @radius < MAX_RADIUS
+      result = make_request
+      search_result = json_result(result)
+      break if search_result['status'] != "ZERO_RESULTS"
+      @radius *= 2
+    end
+
+    search_result
+  end
 
   def url
     URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{@latitude}%2C#{@longitude}&radius=#{@radius}&type=restaurant&key=#{Rails.application.credentials[Rails.env.to_sym][:gmaps][:apikey]}")
@@ -27,10 +38,10 @@ class MapSearchService
     https.use_ssl = true
 
     request = Net::HTTP::Get.new(url)
-    https.request(request).read_body
+    result = https.request(request).read_body
   end
 
   def json_result(request_result)
-    @result.merge(JSON.parse(request_result))
+    JSON.parse(request_result)
   end
 end
